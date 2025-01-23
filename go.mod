b94ffe006a66d2aa9893cd705c0cdf862ad6075c
Daniel N <2color@users.noreply.github.com>
2025-01-23 20:00:35 +0100

    add debug logging

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/libp2p/go-nat
 
 require (
 	github.com/huin/goupnp v1.2.0
+	github.com/ipfs/go-log/v2 v2.5.1
 	github.com/jackpal/go-nat-pmp v1.0.2
 	github.com/koron/go-ssdp v0.0.4
 	github.com/libp2p/go-netroute v0.2.1
@@ -9,6 +10,10 @@ require (
 
 require (
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
--- nat.go
+++ nat.go
@@ -4,11 +4,16 @@ package nat
 import (
 	"context"
 	"errors"
+
 	"math"
 	"math/rand"
 	"net"
 	"time"
+
+	"github.com/ipfs/go-log/v2"
 )
+
+var logger = log.Logger("nat")
 
 var ErrNoExternalAddress = errors.New("no external address")
 var ErrNoInternalAddress = errors.New("no internal address")
@@ -86,20 +91,36 @@ func DiscoverNATs(ctx context.Context) <-chan NAT {
 
 // DiscoverGateway attempts to find a gateway device.
 func DiscoverGateway(ctx context.Context) (NAT, error) {
+	logger.Debugf("Starting NAT gateway discovery")
 	var nats []NAT
 	for nat := range DiscoverNATs(ctx) {
+		natType := nat.Type()
+		addr, _ := nat.GetDeviceAddress()
+		logger.Debugf("Found NAT: type=%s address=%v", natType, addr)
 		nats = append(nats, nat)
 	}
 	switch len(nats) {
 	case 0:
+		logger.Debugf("NAT discovery failed: no NATs found")
 		return nil, ErrNoNATFound
 	case 1:
+		logger.Debugf("NAT discovery succeeded: found single NAT of type %s", nats[0].Type())
 		return nats[0], nil
+	default:
+		logger.Debugf("NAT discovery found multiple NATs (%d), will select best match", len(nats))
 	}
 	gw, _ := getDefaultGateway()
+	if gw != nil {
+		logger.Debugf("Default gateway address: %v", gw)
+	} else {
+		logger.Debugf("No default gateway address found")
+	}
+
 	bestNAT := nats[0]
 	natGw, _ := bestNAT.GetDeviceAddress()
 	bestNATIsGw := gw != nil && natGw.Equal(gw)
+	logger.Debugf("Initial best NAT: type=%s address=%v isGateway=%v", bestNAT.Type(), natGw, bestNATIsGw)
+
 	// 1. Prefer gateways discovered _last_. This is an OK heuristic for
 	// discovering the most-upstream (furthest) NAT.
 	// 2. Prefer gateways that actually match our known gateway address.
@@ -107,14 +128,18 @@ func DiscoverGateway(ctx context.Context) (NAT, error) {
 	for _, nat := range nats[1:] {
 		natGw, _ := nat.GetDeviceAddress()
 		natIsGw := gw != nil && natGw.Equal(gw)
+		logger.Debugf("Comparing NAT: type=%s address=%v isGateway=%v", nat.Type(), natGw, natIsGw)
 
 		if bestNATIsGw && !natIsGw {
+			logger.Debugf("Skipping non-gateway NAT in favor of current gateway NAT")
 			continue
 		}
 
+		logger.Debugf("Selecting new best NAT: type=%s address=%v", nat.Type(), natGw)
 		bestNATIsGw = natIsGw
 		bestNAT = nat
 	}
+	logger.Debugf("Final NAT selection: type=%s address=%v isGateway=%v", bestNAT.Type(), natGw, bestNATIsGw)
 	return bestNAT, nil
 }
 
